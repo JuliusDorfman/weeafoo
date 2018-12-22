c3ff8682806a9b38b8e73471c68a4cf944e4dab7
JuliusDorfman <juliusdorfman@gmail.com>
2018-12-22 13:13:10 -0800

    uncommented production build node block

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(bodyParser.urlencoded({
 const kitsuRoutes = require('./api/routes');
 app.use('/api/routes', kitsuRoutes);
 
-// // Serve static assets if in production build
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/client/build')));
+// Serve static assets if in production build
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
-//   app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
-// };
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+};
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port: ${port}`));
