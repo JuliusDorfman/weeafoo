872fe5848a795cc8be394960ca4c7017615800ee
JuliusDorfman <juliusdorfman@gmail.com>
2019-01-09 19:43:56 -0800

    inialize output css

--- api/routes.js
+++ api/routes.js
@@ -12,94 +12,94 @@ router.get('/test', (req, res) => {
   });
 });
 
-// ROUTE: /api/routes/slug/:name
-// DESC: Search Kitsu API by title and return 1 result
+// ROUTE: /api/routes/keyword/:parameter
+// DESC: Search Tenor by keywords and return list of results
 // ACCESS: Public
 
-router.get('/slug/:name', (req, res) => {
-  axios.get(`https://kitsu.io/api/edge/anime?filter[slug]=${req.params.name}`).then(result => {
-    console.log("result.data.data[0].attributes.titles", result.data.data[0].attributes.titles)
-    
+router.get('/keyword/:keywords', (req, res) => {
+  let key = "HHBE1MKQWCCG";
+  console.log("req.params", req.params)
+  axios.get(`https://api.tenor.com/v1/search?q=${req.params.keywords.toLowerCase()}$key=${key}`).then(result => {
 
-    if (result.data.data[0].attributes.titles) {
-      Object.entries(result.data.data[0].attributes.titles).map((value, index) => {})
+  console.log("RAWDATA", result.data)
 
-      if (result.data.data[0].attributes) {
-        return res.json({
-          animeName: result.data.data[0].attributes
-        })
-      } else {
-        return res.json({
-          noResult: "No results!"
-        })
-      }
-    }
-  }).catch(err => {
-    console.log(err)
-  })
-});
+  let gifArray = [];
+  let gifID = [];
 
-// ROUTE: /api/routes/category/:category
-// DESC: Search Kitsu API by categories and return multiple results
-// ACCESS: Public
-
-router.get('/categories/:category', (req, res) => {
-  axios.get(`https://kitsu.io/api/edge/anime?filter[categories]=${req.params.category}`).then(result => {
-
-    let categoryResult = {};
-    let animeNames = [];
-    let animePosters = [];
-
-    result.data.data.map((value, index) => {
-      let imageValue = value.attributes.posterImage.original
-      let nameValue = value.attributes.slug;
-
-      animePosters.push(imageValue);
-      animeNames.push(nameValue);
+    result.data.results.map((value, index) => {
+      value.media.map((value, index) => {
+        gifArray.push(value.tinygif.url);
+      })
     })
-
-    categoryResult.animeNames = animeNames;
-    categoryResult.animePosters = animePosters;
 
     return res.json({
-      categoryResult: categoryResult,
-      rawData: result.data
+      gifArray: gifArray,
+      gifID: gifID
     })
-  }).catch(err => {
-    console.log(err)
   })
-});
+})
 
-// ROUTE: /api/routes/name/:name
-// DESC: Search Kitsu API by title and return 1 result
-// ACCESS: Public
+module.exports = router;
 
-// router.get('/name/:name', (req, res) => {
-//   axios.get(`https://kitsu.io/api/edge/anime?${req.params.name}`).then(result => {
+
+
+
+
+
+// // ROUTE: /api/routes/slug/:name
+// // DESC: Search Kitsu API by title and return 1 result
+// // ACCESS: Public
+
+// router.get('/slug/:name', (req, res) => {
+//   axios.get(`https://kitsu.io/api/edge/anime?filter[slug]=${req.params.name}`).then(result => {
 //     console.log("result.data.data[0].attributes.titles", result.data.data[0].attributes.titles)
 
+
 //     if (result.data.data[0].attributes.titles) {
+//       Object.entries(result.data.data[0].attributes.titles).map((value, index) => {})
 
-//       Object.entries(result.data.data[0].attributes.titles).map((value, index) => {
-//         console.log("value[i]", value[1].toLowerCase());
-//         console.log("SEARCH PARAMS", req.params.name)
-//       })
-
-//       return res.json({
-//         animeName: result.data.data[0].attributes
-//       })
-
-//       if (result.data.data[0].attributes === undefined) {
+//       if (result.data.data[0].attributes) {
+//         return res.json({
+//           animeName: result.data.data[0].attributes
+//         })
+//       } else {
 //         return res.json({
 //           noResult: "No results!"
 //         })
 //       }
-
 //     }
-
 //   }).catch(err => {
 //     console.log(err)
 //   })
 // });
 
-module.exports = router;
+// // ROUTE: /api/routes/category/:category
+// // DESC: Search Kitsu API by categories and return multiple results
+// // ACCESS: Public
+
+// router.get('/categories/:category', (req, res) => {
+//   axios.get(`https://kitsu.io/api/edge/anime?filter[categories]=${req.params.category}`).then(result => {
+
+//     let categoryResult = {};
+//     let animeNames = [];
+//     let animePosters = [];
+
+//     result.data.data.map((value, index) => {
+//       let imageValue = value.attributes.posterImage.original
+//       let nameValue = value.attributes.slug;
+
+//       animePosters.push(imageValue);
+//       animeNames.push(nameValue);
+//     })
+
+//     categoryResult.animeNames = animeNames;
+//     categoryResult.animePosters = animePosters;
+
+//     return res.json({
+//       categoryResult: categoryResult,
+//       rawData: result.data
+//     })
+//   }).catch(err => {
+//     console.log(err)
+//   })
+// });
--- client/src/App.js
+++ client/src/App.js
@@ -7,155 +7,60 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      slugNameInput: "",
-      categoryNameInput: "",
-      outputVal: [],
-      searchValName: [],
-      searchValImage: [],
-      outputObject: []
+      searchKeyword: "",
+      keywordSubmit: []
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value.toLowerCase()});
+    this.setState({ [e.target.name]: e.target.value.toLowerCase() })
   };
 
   handleSubmit(e) {
     e.preventDefault();
-    if (this.state.slugNameInput) {
-      this.setState({
-        outputVal: document.getElementById('slug-form').value
-      }, () => {
-        axios.get(`/api/routes/slug/${this.state.outputVal.toLowerCase()}`).then(result => {
-          let image = result.data.animeName.posterImage.original;
-          let name = "";
-
-          if (result.data.animeName.titles.en) {
-              name = result.data.animeName.titles.en
-            } else if (result.data.animeName.titles.en_us) {
-              name = result.data.animeName.titles.en_us
-            } else {
-              name = result.data.animeName.titles.en_jp
-          }
-
-          let object = {
-            name: name,
-            image: image
-          }
-
-          // Resetting State from other searches
-          this.setState({slugNameInput: ""});
-
-          // Pushing Values to State
-          this.setState({searchValName: [name]});
-          this.setState({searchValImage: [image]});
-          this.setState({outputObject: [object]}, ()=> {
-            console.log("Name Search Output", this.state.outputObject
-              )})
-        }).catch(err => {
-          console.log(err);
-        })
-      });
-    };
-
-    if (this.state.categoryNameInput) {
-      this.setState({
-        outputVal: document.getElementById('category-form').value
-      }, () => {
-        // Initialize Image Array outside of the axios call
-
-        // Making axios call with user input conditional variable
-        axios.get(`/api/routes/categories/${this.state.outputVal.toLowerCase()}`).then(result => {
-        let nameArray = [];
-        let imgArray = [];
-
-        // Mapping through result.data.categoryResult (categoryResult is the name of the object I gave the return object during the backend Axios call) using Object.Entries
-        result.data.rawData.data.map((value, index) => {
-
-          let name = "";
-
-          if (value.attributes.titles.en) {
-            name = value.attributes.titles.en
-          } else if (value.attributes.titles.en_us) {
-            name = value.attributes.titles.en_us
-          } else {
-            name = value.attributes.titles.en_jp
-        }
-
-        return nameArray.push(name);
+    let keywords = `anime%20${this.state.searchKeyword}%20`;
+    axios.get(`/api/routes/keyword/${keywords}`)
+      .then(result => {
+        console.log("RESULT FROM AXIOS", result.data)
+        this.setState({ keywordSubmit: result.data.gifArray }, () => {
+          console.log("KEYWORD SUBMIT", this.state.keywordSubmit)
         });
-
-        result.data.categoryResult.animePosters.map((value, index) => {
-          return imgArray.push(value);
-        });
-
-        // Resetting State from other searches
-        this.setState({categoryNameInput: ""});
-
-        // Pushing Values to State
-        this.setState({searchValName: nameArray});
-        this.setState({searchValImage: imgArray});
-
-        let arrOfObjects = [];
-
-        nameArray.map((value, index, array) => {
-          let newObject = {};
-          newObject.name = value;
-          newObject.img = imgArray[index];
-          arrOfObjects.push(newObject)
-          return newObject
-        });
-        return this.setState({outputObject: arrOfObjects}, ()=> {
-          console.log("Category Search Output", this.state.outputObject)
-        });
-
-        }).catch(err => {
-          console.log(err);
-        })
-      });
-    };
+      })
+      .catch((err) => { return err })
   };
 
   render() {
 
     let output = (
-      this.state.outputObject.map((value, index) => {
+      this.state.keywordSubmit.map((value, index) => {
+        console.log("value", value);
         return (
-          <div className="anime-wrapper" key={value.name}>
-            <div className="anime-name" >
-              <p>{value.name}</p>
-            </div>
-            <div className="anime-poster-wrapper">
-            {/* Replace PLACEHOLDER src with "value.img" */}
-              <img className="anime-poster" src={`https://sspride.org/wp-content/uploads/2017/03/image-placeholder-500x500.jpg`} alt={value.name}/>
+          <div className="single-output-wrapper" key={value}>
+            <div className="image-wrapper">
+              <img src={value} alt={value} />
             </div>
           </div>
         )
       })
-    );
+    )
 
-    
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            ENTER SEARCH
-          </p>
+        <div className="search-component">
           <form onSubmit={this.handleSubmit}>
-            <input id="slug-form" name="slugNameInput" placeholder="Slug" value={this.state.slugNameInput} type="text" onChange={this.handleChange} autoComplete="off"></input>
-            <input type="submit"></input>
+            <input name="searchKeyword" type="text" placeholder="Tags" value={this.state.searchKeyword} onChange={this.handleChange} autoComplete="off" />
+            <input type="submit" />
           </form>
-          <form onSubmit={this.handleSubmit}>
-            <input id="category-form" name="categoryNameInput" placeholder="Category" value={this.state.categoryNameInput} type="text" onChange={this.handleChange} autoComplete="off"></input>
-            <input type="submit"></input>
-          </form>
-        </header>
-        <div className="anime-output-container">
-          {output}
+          <div className="output-wrapper">
+            {this.state.keywordSubmit ?
+              output
+              :
+              <div>Test</div>
+            }
+          </div>
         </div>
-     
       </div>
     );
   }
